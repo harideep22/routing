c44479cd2103bc0c69eecd6ff8e1fc769809c357
Harideep KS <harideepkshyam2@gmail.com>
2023-12-19 16:54:54 +0530

    dsa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServerComponent } from './server/server.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:HomeComponent
+  },
+  {
+    path:'server',component:ServerComponent
+  },
+  {
+    path:'user',component:UserComponent
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
